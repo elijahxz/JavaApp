84b5e5793aae0b958eef93da0c2480e218b9a381
chris363578 <102829320+chris363578@users.noreply.github.com>
2023-09-14 13:55:30 -0500

    Changed code

--- 2023FallJavaApp/Base.java
+++ 2023FallJavaApp/Base.java
@@ -1,10 +1,23 @@
+import java.awt.*;
+
 
 public class Base {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-	}
+		// Create a frame
+        Frame frame = new Frame("My GUI");
 
+        // Create a label
+        Label label = new Label("This is a label");
+
+        // Add the label to the frame
+        frame.add(label);
+
+        // Set the size of the frame
+        frame.setSize(300, 200);
+
+        // Display the frame
+        frame.setVisible(true);
+    }
 }
