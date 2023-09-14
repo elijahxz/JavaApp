54a1ccbc5c02ddd76ed3baed4486c2a4885b62dd
Alectmc <alectmc@gmail.com>
2023-09-14 14:23:41 -0500

    Added a test button to be configured...

--- 2023FallJavaApp/Base.java
+++ 2023FallJavaApp/Base.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 
 
 public class Base {
@@ -7,17 +8,20 @@ public class Base {
 		// TODO Auto-generated method stub
 		// Create a frame
         Frame frame = new Frame("My GUI");
+        Button closeTester = new Button("Close Me!");
 
         // Create a label
         Label label = new Label("This is a label");
 
         // Add the label to the frame
         frame.add(label);
-
+        
         // Set the size of the frame
         frame.setSize(300, 200);
 
         // Display the frame
         frame.setVisible(true);
+        
+        
     }
 }
