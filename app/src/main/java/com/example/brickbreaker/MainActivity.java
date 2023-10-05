f22f9ffeaf158690c959c21ec2c36795b6809db9
Alectmc <alectmc@gmail.com>
2023-10-05 14:23:16 -0500

    We have a colorful button

--- app/src/main/java/com/example/brickbreaker/MainActivity.java
+++ app/src/main/java/com/example/brickbreaker/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.brickbreaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Button changeColor = new Button(this);
+        changeColor.setText("Color");
+        changeColor.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        changeColor.setOnClickListener(new View.OnClickListener() {
+            int[] colors = {Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW};
+            @Override
+            public void onClick(View view) {
+                final Random random = new Random();
+                view.setBackgroundColor(colors[random.nextInt(colors.length - 1) + 1]);
+            }
+        });
+        setContentView(changeColor);
     }
 }
